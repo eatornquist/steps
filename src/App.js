c5b5aed54b9e62adb03d60161dd64ab0246e53a5
Eduardo Tornquist <eatornquist04@gmail.com>
2023-09-11 10:00:08 -0400

    Adding the "close" fumctionality by a new state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const messages = [
 ]
 
 export default function App() {
-  const [step, setStep] = useState(1) // Destructuring the useState function
+  const [step, setStep] = useState(1) // Destructuring the useState function, used to update the state in React.
+  const [isOpen, setIsOpen] = useState(true)
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1)
@@ -18,31 +19,39 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 && 'active'}>1</div>
-        <div className={step >= 2 && 'active'}>2</div>
-        <div className={step >= 3 && 'active'}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 && 'active'}>1</div>
+            <div className={step >= 2 && 'active'}>2</div>
+            <div className={step >= 3 && 'active'}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handlePrevious} //In this case we dont call the function with () because it will cause to the function get run when the component is loaded
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handleNext} //In this case we dont call the function with () because it will cause to the function get run when the component is loaded
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handlePrevious} //In this case we dont call the function with () because it will cause to the function get run when the component is loaded
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handleNext} //In this case we dont call the function with () because it will cause to the function get run when the component is loaded
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
